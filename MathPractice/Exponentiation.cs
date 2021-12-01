08b74bd6cccf2ed93cde58208eca7cd820398596
synith <synith@live.ca>
2021-12-01 15:10:02 -0700

    exponentiation example

---
+++ MathPractice/Exponentiation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPractice
+{
+    internal class Exponentiation
+    {
+        int baseNumber;
+        int exponentNumber;
+        int result;
+
+        public void ExponentiationExample()
+        {
+            AskForBase();
+            AskForExponent();
+            CalculateResults();
+        }
+        void AskForBase()
+        {
+            bool validNumber = false;
+            while (!validNumber)
+            {
+                Console.WriteLine("Enter the base number...");
+
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    Console.WriteLine($"You entered {num} as the base\n");
+                    baseNumber = num;
+                    validNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine("invalid entry, please try again");
+                }
+            }
+
+        }
+        void AskForExponent()
+        {
+            bool validNumber = false;
+            while (!validNumber)
+            {
+                Console.WriteLine("Enter the exponent number...");
+
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    Console.WriteLine($"You entered {num} as the exponent\n");
+                    exponentNumber = num;
+                    validNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine("invalid entry, please try again");
+                }
+            }
+        }
+        private void CalculateResults()
+        {
+            result = (int)Math.Pow(baseNumber, exponentNumber);
+            Console.WriteLine($"{baseNumber} to the power of {exponentNumber} is {result}");
+        }
+    }
+}
--- MathPractice/Program.cs
+++ MathPractice/Program.cs
@@ -3,7 +3,11 @@
 RequestAnyKey();
 while (!(Console.ReadKey(true).Key == ConsoleKey.Escape))
 {
-    Console.WriteLine("Press 1 to enter Rounding Example.\nPress 2 to enter Remainder Example.\nPress ESC to Exit.\n");
+    Console.WriteLine(
+        "Press 1 to enter Rounding Example.\n" +
+        "Press 2 to enter Remainder Example.\n" +
+        "Press 3 to enter Exponent Example.\n" + 
+        "Press ESC to Exit.\n");
     switch (Console.ReadKey(true).Key)
     {        
         case ConsoleKey.D1:
@@ -16,6 +20,13 @@ while (!(Console.ReadKey(true).Key == ConsoleKey.Escape))
             Console.Write("\n");
             Remainder remainder = new Remainder();
             remainder.RemainderExample();
+            RequestAnyKey();
+            break;
+        case ConsoleKey.D3:
+            Console.Write("\n");
+            Exponentiation exponentiation = new Exponentiation();
+            exponentiation.ExponentiationExample();
+            // Exponent example here
             RequestAnyKey();
             break;
         default:

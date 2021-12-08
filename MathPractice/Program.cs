c5bad811431282b77d53f5115d7e0f989e00f996
synith <synith@live.ca>
2021-12-08 12:23:30 -0700

    Set up sub menu for Factorials, Permutations and Combinations

--- MathPractice/Program.cs
+++ MathPractice/Program.cs
@@ -9,9 +9,12 @@ while (!(Console.ReadKey(true).Key == ConsoleKey.Escape))
         "Press 3 to enter Exponent Example.\n" +
         "Press 4 to enter nth Root Example.\n" +
         "Press 5 to enter Logarithm Example.\n" +
+        "Press 6 to enter Factorials, Permutations and Combinations Example.\n" +
         "Press ESC to Exit.\n");
     switch (Console.ReadKey(true).Key)
-    {        
+    {
+        case ConsoleKey.Escape:
+            return;
         case ConsoleKey.D1:
             Console.Write("\n");
             Rounding rounding = new Rounding();
@@ -42,9 +45,52 @@ while (!(Console.ReadKey(true).Key == ConsoleKey.Escape))
             logarithm.LogExample();
             RequestAnyKey();
             break;
+        case ConsoleKey.D6:
+            Console.Write("\n");
+            FactorialsPermutationsCombinationsMenu();
+            RequestAnyKey();
+            break;
         default:
             break;
     }
 }
 
 void RequestAnyKey() => Console.WriteLine("\nPress any key to continue.\nPress ESC to exit.\n");
+
+void FactorialsPermutationsCombinationsMenu()
+{
+    Console.WriteLine("You've entered the Factorials, Permutations and Combinations sub-menu.");
+    RequestAnyKey();
+    while (!(Console.ReadKey(true).Key == ConsoleKey.Escape))
+    {
+        Console.WriteLine(
+            "Press 1 to enter Factorials Example.\n" +
+            "Press 2 to enter Permutations Example.\n" +
+            "Press 3 to enter Combinations Example.\n" +
+            "Press ESC to Exit.\n");
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.Escape:
+                Console.WriteLine("\nYou've returned to the main menu...");
+                return;
+            case ConsoleKey.D1:
+                Console.Write("\n");
+                // Factorial example
+                RequestAnyKey();
+                break;
+            case ConsoleKey.D2:
+                Console.Write("\n");
+                // Permutations example
+                RequestAnyKey();
+                break;
+            case ConsoleKey.D3:
+                Console.Write("\n");
+                // Combinations example
+                RequestAnyKey();
+                break;
+            default:
+                break;
+        }
+    }
+    Console.WriteLine("\nYou've returned to the main menu...");
+}
